e1696bfdc8b1a6609ec2f96a252f1990f9ffadcc
keshav <keshav_garg@outlook.com>
2016-03-17 00:47:17 +0530

    UI updates

--- popup.js
+++ popup.js
@@ -4,15 +4,10 @@
 	var jf = {
 		init: function() {
 			var self = this;
-			document.getElementsByClassName('validate')[0].addEventListener('click', function() {
-				json = self.getJson();
-				var message = self.validate(json);
-				document.getElementsByClassName('message')[0].innerHTML = message;
-			})
 
 			document.getElementsByClassName('format')[0].addEventListener('click', function() {
 				json = self.getJson();
-				self.jsonWithNoColors(json);
+				self.jsonWithNoColors(self, json);
 			})
 
 			 document.getElementsByClassName('format-with-color')[0].addEventListener('click', function() {
@@ -24,26 +19,37 @@
 			json = document.getElementsByClassName('textarea')[0].value;
 			return json;
 		},
-		validate: function(jsonString) {
-			var message;
+		stringToJson: function(jsonString) {
+			var json;
 			try {
 					var jsonString = jsonString.replace((/([\w]+)(:)/g), "\"$1\"$2");
 					var jsonString = jsonString.replace((/'/g), "\"");
-					var json = JSON.parse(jsonString);
-
-					message = 'JSON is valid';
+					json = JSON.parse(jsonString);
 				}
 			catch(error) {
-				 message = 'JSON is not valid';
+				 json = null;
 			}
 			finally {
-				return message;
+				return json;
 			}
 		},
 		jsonWithNoColors: function(self, json) {
+			var json = self.stringToJson(json);
+			if(!json) {
+				alert('JSON is not valid');
+				return;
+			}
+			json = JSON.stringify(json, null, 4);
 			self.printJson(json);
 		},
 		jsonWithColors: function (self, json) {
+			var json = stringToJson(json);
+			if(!json) {
+				alert('JSON is not valid');
+				return;
+			}
+			json = JSON.stringify(json, null, 4);
+
 			json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 			json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
 				function (match) {
@@ -61,10 +67,12 @@
 					}
 					return '<span class="' + cls + '">' + match + '</span>';
 				});
+				alert('replace json', json);
 			self.printJson(json);
 		},
 		printJson: function (inp) {
-			document.getElementsByClassName('message')[0].appendChild(document.createElement('pre')).innerHTML = inp;
+			$('.textarea').fadeOut();
+			document.getElementsByClassName('result-json')[0].appendChild(document.createElement('pre')).innerHTML = inp;
 		}
 	}
 
